18befd0f4968011b0784f073ef7798ba24ecba64
Zicohh <zicobacker26@gmail.com>
2026-04-28 19:50:45 +0200

    updated previous routes to use consts

--- src/components/BottomNavBar.tsx
+++ src/components/BottomNavBar.tsx
@@ -3,24 +3,25 @@ import PersonSharpIcon from "@mui/icons-material/PersonSharp";
 import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
 import RestaurantSharpIcon from "@mui/icons-material/RestaurantSharp";
 import HomeSharpIcon from "@mui/icons-material/HomeSharp";
+import {ROUTES} from "../types/consts.ts";
 
 export default function BottomNavBar() {
     return (
         <footer className="relative bottom-0 z-200 py-2 w-full bg-[#333737] border-t-2 border-[#565d5d]">
             <nav className="h-full max-w-sm mx-auto justify-evenly flex">
-                <NavLink to="/" className={({ isActive }) => `text-center items-center ${isActive ? 'text-[#F67631]' : 'text-[#ffffff]'}`}>
+                <NavLink to={ROUTES.HOME} className={({ isActive }) => `text-center items-center ${isActive ? 'text-[#F67631]' : 'text-[#ffffff]'}`}>
                     <HomeSharpIcon sx={{ fontSize: 40 }}/>
                     <p className="text-xs">Home</p>
                 </NavLink>
-                <NavLink to="/workouts" className={({ isActive }) => `text-center items-center ${isActive ? 'text-[#F67631]' : 'text-[#ffffff]'}`}>
+                <NavLink to={ROUTES.WORKOUTS} className={({ isActive }) => `text-center items-center ${isActive ? 'text-[#F67631]' : 'text-[#ffffff]'}`}>
                     <FitnessCenterIcon sx={{ fontSize: 40 }}/>
                     <p className="text-xs">Workout</p>
                 </NavLink>
-                <NavLink to="/kcal-tracker" className={({ isActive }) => `text-center items-center ${isActive ? 'text-[#F67631]' : 'text-[#ffffff]'}`}>
+                <NavLink to={ROUTES.CAL_TRACKER} className={({ isActive }) => `text-center items-center ${isActive ? 'text-[#F67631]' : 'text-[#ffffff]'}`}>
                     <RestaurantSharpIcon sx={{ fontSize: 40 }}/>
                     <p className="text-xs">Kcal</p>
                 </NavLink>
-                <NavLink to="/profile" className={({ isActive }) => `text-center items-center ${isActive ? 'text-[#F67631]' : 'text-[#ffffff]'}`}>
+                <NavLink to={ROUTES.PROFILE} className={({ isActive }) => `text-center items-center ${isActive ? 'text-[#F67631]' : 'text-[#ffffff]'}`}>
                     <PersonSharpIcon sx={{ fontSize: 40 }}/>
                     <p className="text-xs">Profile</p>
                 </NavLink>
