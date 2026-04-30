89638c34bd19b08c09bbf594bc66431712a7baf3
Zicohh <zicobacker26@gmail.com>
2026-04-30 14:28:18 +0200

    WorkoutItem Shadows, skeletons and comments!

--- src/components/listItems/WorkoutItem.tsx
+++ src/components/listItems/WorkoutItem.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import {useRef, useState} from "react";
 import {useNavigate} from "react-router-dom";
 import DragIndicatorIcon from "@mui/icons-material/DragIndicator";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
@@ -9,12 +9,18 @@ import API from "../../classes/api.ts";
 import {ROUTES} from "../../types/consts.ts";
 import useNonTargetClick from "../../Hooks/useNonTargetClick.ts";
 import toast from "react-hot-toast";
+import {CSSProperties} from "@mui/material";
 
 interface WorkoutItemProps {
     id: string;
     index: number;
     name: string;
     reloadWorkouts: () => void;
+}
+
+
+const shadowCSS: CSSProperties = {
+    boxShadow: "4px 4px 0px color-mix(in oklab, gray 20%,var(--color-components))"
 }
 
 export default function WorkoutItem({id, index, name, reloadWorkouts}:WorkoutItemProps) {
@@ -26,16 +32,12 @@ export default function WorkoutItem({id, index, name, reloadWorkouts}:WorkoutIte
         navigate(`${ROUTES.WORKOUTS}/${id}`);
     }
 
-    return<li onClick={handleClick} ref={ref} className={"w-full flex flex-row bg-components h-16 items-center justify-between gap-4 rounded"}>
-        <div ref={handleRef} className={"bg-components-hover h-full flex items-center rounded-l"}><DragIndicatorIcon className={"text-accent"}/></div>
+    return<li onClick={handleClick} ref={ref} style={shadowCSS} className={"w-full flex flex-row bg-components h-16 items-center justify-between gap-4 rounded"}>
+        <div ref={handleRef} className={"bg-components-hover h-full flex items-center rounded-l"} ><DragIndicatorIcon className={"text-accent"}/></div>
         <span className={"flex-1 rounded-full w-full h-6 text-textcolor font-semibold text-xl"}>{name}</span>
         <DropDownOptions reloadWorkouts={reloadWorkouts} id={id}/>
     </li>
 }
-
-
-
-
 
 interface DropDownOptionsProps {
     reloadWorkouts: () => void,
@@ -45,11 +47,13 @@ function DropDownOptions({reloadWorkouts, id}: DropDownOptionsProps) {
     const [openDropDown,setOpenDropDown] = useState<boolean>(false);
     const dropdownRef = useRef<HTMLDivElement|null>(null);
 
-    // wrapper for clicking outside a certain element.
+    // wrapper for clicking outside a certain element. Closes menu when not clicking on the element.
     useNonTargetClick(() => {
         if(!openDropDown) setOpenDropDown(false);
     }, dropdownRef);
 
+    // Deletes workout, closes dropdown,
+    // and reloads workouts, thus removing the workout from the list as well.
     const handleDelete = () => {
         API.workouts.remove(id)
             .then((resp) => toast.success("removed workout: " + resp));
@@ -59,11 +63,19 @@ function DropDownOptions({reloadWorkouts, id}: DropDownOptionsProps) {
 
     }
 
+    // Just closes dropDown
+    // TODO add edit functionality.
     const handleEdit = () => {
         setOpenDropDown(false);
     }
 
-    return <div className="ml-auto cursor-pointer relative text-textcolor" onClick={() => setOpenDropDown((prev) => !prev)}>
+    // opens the modal. stopPropagation() prevents firing of other functions under it.
+    const handleOpen = (event: React.MouseEvent<HTMLDivElement>) => {
+        event.stopPropagation();
+        setOpenDropDown(true);
+    }
+
+    return <div className="ml-auto cursor-pointer relative text-textcolor" onClick={handleOpen}>
         <MoreVertIcon sx={{ fontSize: 40 }} />
         <div ref={dropdownRef} className={`absolute z-10 top-full right-1 mt-1 flex flex-col rounded-xl p-2 bg-components border border-bordercolor transform transition-all duration-100 ease-out origin-top-right
                  ${openDropDown ? "opacity-100 scale-100" : "opacity-0 scale-95 -translate-y-2 pointer-events-none"}`}>
